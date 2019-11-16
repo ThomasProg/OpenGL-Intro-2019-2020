d5a72cfc02e7e67a0055d4b2ec90e20f1afbc6ac
Thomas DALLARD <t.dallard@debian>
2019-11-16 14:10:58 -0600

    ghostcode removed

--- ex_06/src/main.cpp
+++ ex_06/src/main.cpp
@@ -1,13 +1,5 @@
 #include <cstdio>
 
-// #include "macros.h"
-// #include "mesh.h"
-// #include "sphere.h"
-// #include "cube.h"
-// #include "utilities.h"
-// #include "camera.h"
-// #include "maze.h"
-
 #include "game.h"
 
 int main()
